191a6266358c708d02e3dff37b9c0a5c003d9b65
Sanjai <sanjai63698@gmail.com>
2026-04-21 00:20:24 +0530

    fix footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,7 +4,6 @@ import payment from '../assets/payment.png'
 
 
 export default function Footer() {
-<<<<<<< HEAD
 return (
   <div className='bg-[#dcfff9]'>
 
@@ -76,83 +75,4 @@ return (
     </div>
   </div>
 )
-=======
-  return (
-
-    <div className='bg-[#dcfff9]'>
-
-      
-
-      <div className='container mx-auto '>
-        {/*Footer 1  */}
-        <div className=' w-full h-auto flex flex-col sm:flex-row sm:justify-around px-4 py-10 border-b border-gray-300 '>
-
-          <div className='flex flex-col gap-4'>
-            <div>
-              <img className='cursor-pointer'
-                src={logo} alt="Logo" />
-            </div>
-            <div  >
-              <ul className='text-gray-500 flex flex-col gap-2 cursor-pointer'>
-                <li>Track Order</li>
-                <li>Delivery Returns</li>
-                <li>Warranty</li>
-              </ul>
-            </div>
-          </div>
-          <div className='flex flex-col gap-4'>
-            <div>
-
-              <h2 className='font-semibold'>About us</h2>
-            </div>
-            <div>
-
-              <ul className='text-gray-500 flex flex-col gap-2 cursor-pointer'><li>Rave's Story</li>
-                <li>Work With Us</li>
-                <li>Corporate News</li>
-                <li>Investors</li></ul>
-            </div>
-          </div>
-          <div className='flex flex-col gap-4'>
-            <div >
-
-              <h2 className='font-semibold'>Online Shop</h2>
-            </div>
-            <div>
-
-              <ul className='text-gray-500 flex flex-col gap-2 cursor-pointer '><li>Furniture</li>
-                <li>Decoration</li>
-                <li>Kitchen</li>
-                <li>Interior</li></ul>
-            </div>
-          </div>
-          <div className='flex flex-col gap-4'>
-            <div>
-
-              <h2 className='font-semibold'>Useful Links</h2>
-            </div>
-            <div>
-
-              <ul className='text-gray-500 flex  flex-col gap-2 cursor-pointer'><li>Secure Payment</li>
-                <li>Privacy Policy</li>
-                <li>Terms Of Use</li>
-                <li>Archived Products</li></ul>
-            </div>
-          </div>
-
-        </div>
-
-        {/* Footer 2 */}
-        <div className='flex flex-col items-center lg:flex-row  justify-between py-4 '>
-          <div>
-            <p className='text-gray-500'>&copy;2026 <span className='text-black'>Marketo</span> All Rights reserved</p>
-          </div>
-          <div >
-            <img className='cursor-pointer' src={payment} alt="" />
-          </div>
-        </div>
-      </div>
-    </div>
-  )
->>>>>>> 2df63a12abbe53a87fb1c9873c4344466c3f5b0f
 }
