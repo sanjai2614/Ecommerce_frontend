491cf47c816cfd329e6a82cccd8471a9d36b9c33
Sanjai <sanjai63698@gmail.com>
2026-04-21 19:30:13 +0530

    env remove

--- src/api/axiosInstance.jsx
+++ src/api/axiosInstance.jsx
@@ -1,7 +1,8 @@
 import axios from "axios"
+const URL = import.meta.env.VITE_API_URL
 
 const axiosInstance =axios.create({
-    baseURL:"https://ecommerce-server-pjl4.onrender.com",
+    baseURL:URL,
     headers:{"Content-Type":"application/json"}
 })
 
