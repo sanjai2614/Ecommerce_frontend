d3645f9f47a1792e26c73fd8a87c1cc74257d42a
Sanjai <sanjai63698@gmail.com>
2026-04-21 20:01:22 +0530

    add url

--- src/api/axiosInstance.jsx
+++ src/api/axiosInstance.jsx
@@ -2,7 +2,7 @@ import axios from "axios"
 const URL = import.meta.env.VITE_API_URL
 
 const axiosInstance =axios.create({
-    baseURL:URL,
+    baseURL:"https://ecommerce-server-pjl4.onrender.com",
     headers:{"Content-Type":"application/json"}
 })
 
